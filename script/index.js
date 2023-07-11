96da12c9887f7517abe04298cac782a67892b2cc
Mir2226 <gmihran@internet.ru>
2023-07-11 15:38:01 +0400

    Change JS code and add background image

--- script/index.js
+++ script/index.js
@@ -1,9 +1,12 @@
 // Array of cities
 const cities = ['London', 'New York', 'Paris', 'Tokyo', 'Sydney', 'Rome', 'Cairo', 'Moscow', 'Dubai', 'Rio de Janeiro'];
+let guessedCities = 0; // Counter for the number of guessed cities
+let correctGuesses = 0; // Counter for the number of correct guesses
 // Function to fetch temperature for a city
 async function fetchTemperature(city) {
   const apiKey = '9cff733aee57cb05b63dd4f731c46bc4';
   try {
+    // Fetch temperature data from OpenWeatherMap API
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
     const data = await response.json();
     const temperature = data.main.temp;
@@ -27,12 +30,14 @@ function checkGuess(guess, temperature) {
 function updateDisplay(city, guess, temperature, isCorrect) {
   const result = document.createElement('div');
   result.className = isCorrect ? 'correct' : 'incorrect';
+  // Set the HTML content of the result div
   result.innerHTML = `
     <p>City: ${city}</p>
     <p>Guess: ${guess}</p>
     <p>Temperature: ${temperature}°C</p>
   `;
   const mainSection = document.getElementById('main_section');
+  // Append the result div to the main section of the HTML
   mainSection.appendChild(result);
 }
 // Function to handle the guess
@@ -40,49 +45,63 @@ function handleGuess() {
   const input = document.querySelector('.second_section input');
   const guess = parseInt(input.value);
   input.value = '';
-  const city = cities[Math.floor(Math.random() * cities.length)];
+  const cityNameElement = document.querySelector('.second_section p');
+  // Get the current city name from the HTML
+  const city = cityNameElement.textContent;
+  // Fetch the temperature for the current city
   fetchTemperature(city)
     .then(temperature => {
+      // Check if the guess is correct
       const isCorrect = checkGuess(guess, temperature);
+      // Update the display based on the guess result
       updateDisplay(city, guess, temperature, isCorrect);
+      // Update the counters
+      guessedCities++;
+      if (isCorrect) {
+        correctGuesses++;
+      }
+      // Check if the game is over
+      if (guessedCities === 5) {
+        // Determine the result of the game
+        let gameResult;
+        let emoji;
+        if (correctGuesses >= 3) {
+          gameResult = 'You won!';
+          emoji = '&#127941';
+        } else if (correctGuesses < 4) {
+          gameResult = 'You lost!';
+          emoji = '&#10060';
+        } else {
+          gameResult = "It's a tie!";
+        }
+        // Display the game result and emoji
+        const result = document.createElement('div');
+        result.innerHTML = `<h2>${gameResult} ${emoji}</h2>`;
+        const mainSection = document.getElementById('main_section');
+        mainSection.appendChild(result);
+        // Disable the input and check button
+        input.disabled = true;
+        checkButton.disabled = true;
+      } else {
+        // Select a new random city for the next round
+        const nextCity = cities[Math.floor(Math.random() * cities.length)];
+        // Update the city name in the HTML
+        setCityName(nextCity);
+      }
     })
     .catch(error => {
       console.error('Error:', error);
     });
 }
+// Function to set the city name in the HTML
+function setCityName(city) {
+  const cityNameElement = document.querySelector('.second_section p');
+  // Set the text content of the city name element
+  cityNameElement.textContent = city;
+}
+// Update the city name in the HTML with a random city
+const randomCity = cities[Math.floor(Math.random() * cities.length)];
+setCityName(randomCity);
 // Add event listener to the "Check" button
 const checkButton = document.querySelector('button');
 checkButton.addEventListener('click', handleGuess);
-// Function to set the city name in the HTML
-function setCityName(city) {
-    const cityNameElement = document.querySelector('.second_section p');
-    cityNameElement.textContent = city;
-  }
-  // Update the setCityName function call with a random city
-  const randomCity = cities[Math.floor(Math.random() * cities.length)];
-  setCityName(randomCity);
-  // Function to update the city name based on guess result
-function updateCityName(isCorrect) {
-    const cityNameElement = document.querySelector('.second_section p');
-    cityNameElement.textContent = isCorrect ? 'Correct!' : 'Incorrect!';
-  }
-
-  function handleGuess() {
-    const input = document.querySelector('.second_section input');
-    const guess = parseInt(input.value);
-    input.value = '';
-    const city = cities[Math.floor(Math.random() * cities.length)];
-    fetchTemperature(city)
-      .then(temperature => {
-        const isCorrect = checkGuess(guess, temperature);
-        updateDisplay(city, guess, temperature, isCorrect);
-        updateCityName(isCorrect);
-        setTimeout(() => {
-          const nextCity = cities[Math.floor(Math.random() * cities.length)];
-          setCityName(nextCity);
-        }, 2000); // Delay for 2 seconds before displaying the next city name
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }
